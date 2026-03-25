f2eaccdbd5bb3f58326106c3679e568cb21c488f
geunig <toje9991@gmail.com>
2026-03-25 16:45:45 +0900

    4주차

--- 4week.py
+++ 4week.py
@@ -1,2 +1,8 @@
-for _ in range(5):
-    print('Welcome to everyone!')
+score = [87,84,95,67,88,94,63]
+names = ['영호','순자','영식','순희', score]
+adbook = [['영호','25','010-1234-0000'],['철수','23','010-1111-0000']]
+for i in adbook:
+    print(i)
+
+ri = list(range(5))
+print(ri)
